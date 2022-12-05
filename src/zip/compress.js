5c71bf129d08696b995d34862020ed787dded86a
katyasamuseva <katsiaryna@bluetape.com>
2022-12-06 00:03:55 +0100

    feat: implement zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
+import * as fs from 'node:fs';
+import {createGzip} from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+
+    const readStream = fs.createReadStream('./src/zip/files/fileToCompress.txt');
+    const writeStream = fs.createWriteStream('./src/zip/files/archive.gz');
+
+    readStream
+        .pipe(createGzip())
+        .pipe(writeStream)
+        .on('finish', () => {
+            process.stdout.write(
+                'File is compressed'
+            );
+        });
 };
 
 await compress();
